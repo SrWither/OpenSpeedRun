f2c4e1abc71bb5c22a97dad11514578541e36a12
Fadel <59105868+SrWither@users.noreply.github.com>
2025-07-02 23:23:25 -0300

    some icons in help

--- src/app/help_window.rs
+++ src/app/help_window.rs
@@ -1,5 +1,5 @@
-use eframe::egui;
 use crate::app::state::AppState;
+use eframe::egui;
 
 impl AppState {
     pub fn draw_help_window(&mut self, ctx: &egui::Context) {
@@ -10,7 +10,10 @@ impl AppState {
                 .anchor(egui::Align2::CENTER_CENTER, egui::vec2(0.0, 0.0))
                 .show(ctx, |ui| {
                     ui.vertical_centered(|ui| {
-                        ui.heading("⌨ Keyboard Shortcuts");
+                        ui.heading(format!(
+                            "{} Keyboard Shortcuts",
+                            egui_phosphor::regular::KEYBOARD
+                        ));
                         ui.separator();
                         ui.label("[Space] Start");
                         ui.label("[P] Pause");
@@ -18,8 +21,14 @@ impl AppState {
                         ui.label("[Enter] Split");
                         ui.label("[Ctrl + S] Save PB");
                         ui.label("[Ctrl + Z] Undo last split");
-                        ui.label("[<-] Previous page");
-                        ui.label("[->] Next page");
+                        ui.label(format!(
+                            "[{}] Previous page",
+                            egui_phosphor::regular::ARROW_LEFT
+                        ));
+                        ui.label(format!(
+                            "[{}] Next page",
+                            egui_phosphor::regular::ARROW_RIGHT
+                        ));
                         ui.label("[H] Toggle this help");
 
                         ui.add_space(10.0);
