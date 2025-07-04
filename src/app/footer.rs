47a74c8555b2e77dfac6d12573d8d4d5a4146da5
Fadel <59105868+SrWither@users.noreply.github.com>
2025-07-03 22:52:31 -0300

    previous segment

--- src/app/footer.rs
+++ src/app/footer.rs
@@ -15,6 +15,11 @@ impl AppState {
 
         let info_color = Color32::from_hex(&colors.info).unwrap_or(Color32::WHITE);
 
+        let gold_positive_color = Color32::from_hex(&colors.gold_positive).unwrap_or(Color32::GOLD);
+        let gold_negative_color = Color32::from_hex(&colors.gold_negative).unwrap_or(Color32::RED);
+        let pb_positive_color = Color32::from_hex(&colors.pb_positive).unwrap_or(Color32::GREEN);
+        let pb_negative_color = Color32::from_hex(&colors.pb_negative).unwrap_or(Color32::RED);
+
         let sum_of_bests = self
             .splits_backup
             .iter()
@@ -48,6 +53,47 @@ impl AppState {
             .collect::<Option<Vec<_>>>()
             .map(|times| times.into_iter().sum());
 
+        let previous_split_relative = if self.current_split > 0 {
+            let current = self
+                .splits_display
+                .get(self.current_split - 1)
+                .and_then(|s| s.last_time)
+                .unwrap_or(Duration::zero());
+
+            let previous = self
+                .splits_display
+                .get(self.current_split - 2)
+                .and_then(|s| s.last_time)
+                .unwrap_or(Duration::zero());
+
+            current - previous
+        } else {
+            Duration::zero()
+        };
+
+        let previous_segment_pb = self
+            .splits_display
+            .get(self.current_split.saturating_sub(1))
+            .and_then(|s| s.pb_time)
+            .unwrap_or(Duration::zero());
+
+        let previous_segment_gold = self
+            .splits_display
+            .get(self.current_split.saturating_sub(1))
+            .and_then(|s| s.gold_time)
+            .unwrap_or(Duration::zero());
+
+        let delta_vs_pb = previous_split_relative - previous_segment_pb;
+        let delta_vs_gold = previous_split_relative - previous_segment_gold;
+
+        let format_diff = |diff: Duration| -> String {
+            let sign = if diff < Duration::zero() { "-" } else { "+" };
+            let ms = diff.num_milliseconds().abs();
+            let secs = ms / 1000;
+            let rem_ms = ms % 1000;
+            format!("{}{:02}.{:03}", sign, secs, rem_ms)
+        };
+
         egui::TopBottomPanel::bottom("footer")
             .resizable(false)
             .min_height(64.0)
@@ -75,38 +121,89 @@ impl AppState {
                         )
                     };
                     ui.add_space(4.0);
-                    ui.vertical_centered(|ui| {
-                        ui.label(
-                            RichText::new(format!(
-                                "{} Sum of Best: {}",
-                                egui_phosphor::regular::FLAG_CHECKERED,
-                                format_dur(sum_of_bests)
-                            ))
-                            .color(info_color)
-                            .size(font_sizes.info),
-                        );
-
-                        ui.label(
-                            RichText::new(format!(
-                                "{} Best Possible: {}",
-                                egui_phosphor::regular::GAUGE,
-                                format_dur(best_possible_time)
-                            ))
-                            .color(info_color)
-                            .size(font_sizes.info),
-                        );
-
-                        if let Some(pb) = pb_time {
+                    ui.vertical(|ui| {
+                        ui.horizontal(|ui| {
+                            ui.add_space(8.0);
                             ui.label(
                                 RichText::new(format!(
-                                    "{} PB: {}",
-                                    egui_phosphor::regular::CHART_POLAR,
-                                    format_dur(pb)
+                                    "{} Sum of Best: {}",
+                                    egui_phosphor::regular::FLAG_CHECKERED,
+                                    format_dur(sum_of_bests)
                                 ))
                                 .color(info_color)
                                 .size(font_sizes.info),
                             );
+                        });
+
+                        ui.horizontal(|ui| {
+                            ui.add_space(8.0);
+                            ui.label(
+                                RichText::new(format!(
+                                    "{} Best Possible: {}",
+                                    egui_phosphor::regular::GAUGE,
+                                    format_dur(best_possible_time)
+                                ))
+                                .color(info_color)
+                                .size(font_sizes.info),
+                            );
+                        });
+
+                        if let Some(pb) = pb_time {
+                            ui.horizontal(|ui| {
+                                ui.add_space(8.0);
+                                ui.label(
+                                    RichText::new(format!(
+                                        "{} PB: {}",
+                                        egui_phosphor::regular::CHART_POLAR,
+                                        format_dur(pb)
+                                    ))
+                                    .color(info_color)
+                                    .size(font_sizes.info),
+                                );
+                            });
                         }
+
+                        let (delta_text, delta_icon, delta_value, delta_color) =
+                            if self.run.gold_split {
+                                (
+                                    "Prev Gold Segment",
+                                    egui_phosphor::regular::STAR,
+                                    delta_vs_gold,
+                                    if delta_vs_gold < Duration::zero() {
+                                        gold_positive_color
+                                    } else {
+                                        gold_negative_color
+                                    },
+                                )
+                            } else {
+                                (
+                                    "Prev PB Segment",
+                                    egui_phosphor::regular::ARROW_LINE_UP,
+                                    delta_vs_pb,
+                                    if delta_vs_pb < Duration::zero() {
+                                        pb_positive_color
+                                    } else {
+                                        pb_negative_color
+                                    },
+                                )
+                            };
+                        if self.current_split > 0 {
+                            ui.horizontal(|ui| {
+                                ui.add_space(8.0);
+                                ui.label(
+                                    RichText::new(format!(
+                                        "{} {}: {}",
+                                        delta_icon,
+                                        delta_text,
+                                        format_diff(delta_value)
+                                    ))
+                                    .color(delta_color)
+                                    .size(font_sizes.info),
+                                );
+                            });
+                        }
+
+                        ui.add_space(4.0);
                     });
                 }
             });
